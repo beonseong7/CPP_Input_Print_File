c07c58931d4c63ce6f58dd79a896634737028630
beonseong7 <beonseong7@naver.com>
2023-02-01 15:39:14 +0900

    2023.02.01 binary file2 function

--- CPP_random/CPP_random/random.cpp
+++ CPP_random/CPP_random/random.cpp
@@ -39,4 +39,32 @@ int main()
 			exit(EXIT_FAILURE);
 		}
 	}
+	else
+	{
+		cerr << file << "파일을 열 수 없습니다. 종료\n";
+		exit(EXIT_FAILURE);
+	}
+	cout << "수정할 레코드 번호를 입력하십시오: ";
+	long rec;
+	cin >> rec;
+	eatline();
+	if (rec < 0 || rec >= ct)
+	{
+		cerr << "자롬ㅅ된 레코드 번호입니다.종류\n";
+		exit(EXIT_FAILURE);
+	}
+	streampos place = rec * sizeof pl;
+	finout.seekg(place);
+	if (finout.fail())
+	{
+		cerr << "레코드를 찾다가 에러 발생.\n";
+		exit(EXIT_FAILURE);
+	}
+	finout.read((char*)&pl, sizeof pl);
+	cout << "현재 레코드의 내용:\n";
+	cout << rec << ": " << setw(LIM) << pl.name << ": "
+		<< setprecision(0) << setw(12) << pl.population
+		<< setprecision(2) << setw(6) << pl.g << endl;
+	if (finout.eof())
+		finout.clear();
 }
